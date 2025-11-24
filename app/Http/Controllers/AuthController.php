a7da9636c2aabeddcfd4727b8b0ce2f09262920d
Gcc <iliashadravan84@gmail.com>
2025-11-25 01:32:06 +0330

    Authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $request->validated();
+        $user = User::create([
+            'full_name' => $request->get('full_name'),
+            'email' => $request->get('email'),
+            'phone'=> $request->get('phone'),
+            'password' => Hash::make($request->get('password')),
+        ]);
+        return response()->json([
+           'success' => true,
+           'data' => $user,
+           'massage'=> 'user created successfully'
+        ]);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $data = $request->validated();
+
+        $user = User::where('phone', $data['phone'])->first();
+
+        if (!$user || !Hash::check($data['password'], $user->password)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid phone or password'
+            ], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'token' => $token,
+            'user' => $user,
+            'message' => 'Login successful'
+        ]);
+    }
+
+
+    public function me()
+    {
+        return response()->json([
+            'success' => true,
+            'user' => auth()->user(),
+        ]);
+    }
+
+    public function logout()
+    {
+        auth()->user()->currentAccessToken()->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Logout successful'
+        ]);
+    }
+}
---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+
+use App\Http\Requests\Request;
+
+class RegisterRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'full_name' => ['required', 'string', 'max:255'],
+            'email' => ['nullable', 'string', 'email', 'max:255', 'unique:users'],
+            'phone' => ['required', 'string', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:4'],
+        ];
+    }
+}
